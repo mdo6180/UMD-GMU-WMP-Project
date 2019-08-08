d50530447d9eebefe78785de9d5a1a6ab969f17c
Minh Quan Do <mdo6180@gmail.com>
2019-08-08 16:07:16 -0400

    deleted some comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,12 @@ int main(){
     features.push_back(-0.465148);
     features.push_back(-0.495225);
     
-	//double result = predict(1.483887, 1.865988, 2.234620, 1.018782, -2.530891, -1.604642, 0.774676, -0.465148, -0.495225);
-    double result = predict(features);
+	double result = predict(features);
 	std::cout << "result is " << result << std::endl;
-    //cleanup();
     
     double answer = predict(features);
     std::cout << "answer is " << answer << std::endl;
+    
     cleanup();
     
 	return 0;
